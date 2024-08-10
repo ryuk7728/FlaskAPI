ce45d4b4b08831a2d12ed2e96998f6910dcb8fb8
Pranay Agrawal <ryuk7728@gmail.com>
2024-08-10 23:49:18 +0530

    Update dataGen.py

--- dataGen.py
+++ dataGen.py
@@ -8,7 +8,7 @@ from email.mime.multipart import MIMEMultipart
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_parameters():
     k = 3
     api_input = request.args.get('type')
@@ -308,4 +308,4 @@ def get_parameters():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host = "0.0.0.0",port=5000,debug=True)
